e6a91d5cd78e0569b38d3db660783d555caa897a
Daniel Sarmiento <73312280@continental.edu.pe>
2023-05-24 03:53:30 -0500

    Intente subir en el anterior repositorio pero me salta un error con el push ,en este commit confirmo que subi la quinta etapa ,para poder correrlo seria necesario cambiar el jdk a una version 8 y compilar las clases.Daniel Fuera

--- src/ch/makery/address/util/LocalDateAdapter.java
+++ src/ch/makery/address/util/LocalDateAdapter.java
@@ -4,10 +4,30 @@
  */
 package ch.makery.address.util;
 
+
+import java.time.LocalDate;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import java.time.LocalDate;
+
+import java.time.LocalDate;
+
 /**
- *
- * @author USUARIO
+ * Adapter (for JAXB) to convert between the LocalDate and the ISO 8601 
+ * String representation of the date such as '2012-12-03'.
+ * 
+ * @author Marco Jakob
  */
-public class LocalDateAdapter {
-    
+public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+
+	@Override
+	public LocalDate unmarshal(String v) throws Exception {
+		return LocalDate.parse(v);
+	}
+
+	@Override
+	public String marshal(LocalDate v) throws Exception {
+		return v.toString();
+	}
 }
